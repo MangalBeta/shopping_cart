a591b85054f645d1e7c90794b311ba7885b1cb3a
mangal <mangalhcl449@gmail.com>
2016-06-18 11:39:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 //require("./cart_init");
 //*****connect the database******
 var mongoose=require("mongoose");
-mongoose.connect('mongodb://localhost/Shopping_Cart');
+mongoose.connect('mongodb://shopping_cart_mangal.herokuapp.com/Shopping_Cart');
 
 var db=mongoose.connection;
 db.on('error',function(err,data){
